9a51d76bcdfe1538a1686bb86b5e711a4773dc7b
Matt Wamboldt <mattwamboldt@hotmail.com>
2011-06-25 10:30:21 -0300

    The beginnings of UI animation
    Every shape will have an animation which is a list of timed states(keyframes).
    Every frame of the game, each playing animation will be advanced 1 frame,
    which is interpolating the state values between the current frame and the next keyframe.
    
    Currently this is only a class implementation that is on its own. Next will
    be to hook these into the game and content pipeline, along with adding
    convenience functions for playback and such.

---
+++ xna/BoardGameContent/UI/Animation.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BoardGameContent.UI
+{
+    //This class stores frames and is responsible for interpolating between them
+    public class Animation
+    {
+        private int mCurrentKeyFrame;
+        [ContentSerializerIgnore]
+        public int CurrentKeyFrame { get { return mCurrentKeyFrame; } set { mCurrentKeyFrame = value; } }
+
+        private ShapeState mCurrentFrame;
+        [ContentSerializerIgnore]
+        public ShapeState CurrentFrame { get { return mCurrentFrame; } set { mCurrentFrame = value; } }
+
+        protected ShapeState[] mKeyFrames; //Defines position as an offset from the parent
+        public ShapeState[] KeyFrames{ get { return mKeyFrames; } set { mKeyFrames = value; } }
+
+        /// <summary>
+        /// Calculates the next frame based on the current frame and next keyframe
+        /// </summary>
+        public void NextFrame()
+        {
+            int nextKeyFrameIndex = CurrentKeyFrame + 1;
+            
+            // we hit the end of the animation so stop here
+            if (nextKeyFrameIndex < mKeyFrames.Length)
+            {
+                ShapeState nextKeyFrame = mKeyFrames[nextKeyFrameIndex];
+                int framesBetween = nextKeyFrame.Frame - mCurrentFrame.Frame;
+
+                if (framesBetween == 0)
+                {
+                    mCurrentFrame = new ShapeState(nextKeyFrame);
+                }
+                else
+                {
+                    mCurrentFrame.Position = CalculateVectorChange(mCurrentFrame.Position, nextKeyFrame.Position, framesBetween);
+                    mCurrentFrame.Size = CalculateVectorChange(mCurrentFrame.Size, nextKeyFrame.Size, framesBetween);
+                    mCurrentFrame.Color = CalculateColorChange(mCurrentFrame.Color, nextKeyFrame.Color, framesBetween);
+                    mCurrentFrame.Frame += 1;
+                }
+            }
+        }
+
+        private Color CalculateColorChange(Color start, Color end, int framesBetween)
+        {
+            //may want to Lerp later but seems fine for now
+            Color returnColor = start;
+            returnColor.A += (byte)((end.A - start.A) / framesBetween);
+            returnColor.R += (byte)((end.R - start.R) / framesBetween);
+            returnColor.G += (byte)((end.G - start.G) / framesBetween);
+            returnColor.B += (byte)((end.B - start.B) / framesBetween);
+            return returnColor;
+        }
+
+        private Vector2 CalculateVectorChange(Vector2 start, Vector2 end, int framesBetween)
+        {
+            Vector2 returnValue = start;
+            returnValue.X += (end.X - start.X) / framesBetween;
+            returnValue.Y += (end.Y - start.Y) / framesBetween;
+            return returnValue;
+        }
+    }
+
+    public class AnimationReader : ContentTypeReader<Animation>
+    {
+        protected override Animation Read(ContentReader input, Animation existingInstance)
+        {
+            Animation anim = new Animation();
+
+            anim.KeyFrames = input.ReadObject<ShapeState[]>();
+            anim.CurrentFrame = new ShapeState(anim.KeyFrames[0]);
+            anim.CurrentKeyFrame = 0;
+
+            return anim;
+        }
+    }
+}
---
+++ xna/BoardGameContent/UI/ShapeState.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace BoardGameContent.UI
+{
+    //These are factors of a shape that can change over time in animations
+    public class ShapeState
+    {
+        protected Vector2 mPosition; //Defines position as an offset from the parent
+        public Vector2 Position { get { return mPosition; } set { mPosition = value; } }
+
+        protected Vector2 mSize;
+        public Vector2 Size { get { return mSize; } set { mSize = value; } }
+
+        protected Color mColor;
+        public Color Color { get { return mColor; } set { mColor = value; } }
+
+        protected int mFrame;
+        public int Frame { get { return mFrame; } set { mFrame = value; } }
+
+        public ShapeState() { }
+        public ShapeState(ShapeState orginal)
+        {
+            Position = orginal.Position;
+            Size = orginal.Size;
+            Color = orginal.Color;
+            Frame = orginal.Frame;
+        }
+    }
+
+    public class ShapeStateReader : ContentTypeReader<ShapeState>
+    {
+        protected override ShapeState Read(ContentReader input, ShapeState existingInstance)
+        {
+            ShapeState state = new ShapeState();
+
+            state.Position = input.ReadVector2();
+            state.Size = input.ReadVector2();
+            state.Color = input.ReadColor();
+            state.Frame = input.ReadInt32();
+
+            return state;
+        }
+    }
+}
